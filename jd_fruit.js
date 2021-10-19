af17a13507945b4e96f687bf04270375830f5861
Xaje Yu <xajeyu@gmail.com>
2021-10-19 20:23:20 +0800

    Fix: bug

--- jd_fruit.js
+++ jd_fruit.js
@@ -28,7 +28,12 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-  //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
+  //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开   助力未加入助力池的账号
+  '75ba2ceffb0b40d590df81c378c617db@65bd45580cd94034a1b82e77d4e557eb',
+  '75ba2ceffb0b40d590df81c378c617db@65bd45580cd94034a1b82e77d4e557eb',
+  '75ba2ceffb0b40d590df81c378c617db@65bd45580cd94034a1b82e77d4e557eb',
+  '75ba2ceffb0b40d590df81c378c617db@65bd45580cd94034a1b82e77d4e557eb',
+  '75ba2ceffb0b40d590df81c378c617db@65bd45580cd94034a1b82e77d4e557eb',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = $.isNode() ? (process.env.retainWater ? process.env.retainWater : 100) : ($.getdata('retainWater') ? $.getdata('retainWater') : 100);//保留水滴大于多少g,默认100g;
@@ -1371,9 +1376,9 @@ function readShareCode() {
 // }
 function shareCodesFormat() {
   return new Promise(async resolve => {
+    console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
-      console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
       newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
