0780fdb61a9770a869081441e9a6a4725c8b1351
Xaje Yu <xajeyu@gmail.com>
2021-11-18 14:27:33 +0800

    feat: finish notify

--- sendNotify.js
+++ sendNotify.js
@@ -260,8 +260,8 @@ async function sendNotify(text, desp, params = {}, author = "xajeyu") {
     iGotNotify(text, desp, params), //iGot
     gobotNotify(text, desp), //go-cqhttp
   ]
-  
-  if (IMPORTA_NTNOTICE_SCRIPTS.some(item => text.indexOf(item) !== -1) && IMPORTA_NTNOTICE_URL && IMPORTA_NTNOTICE_TARGET) {
+  const teamFlag = IMPORTA_NTNOTICE_SCRIPTS.some(item => text.indexOf(item) !== -1) || desp.match(/(已可)((兑换)|(领取))/)
+  if (teamFlag && IMPORTA_NTNOTICE_URL && IMPORTA_NTNOTICE_TARGET) {
     basePush.push(JavaScriptTeamNotify(text, desp))
   }
 
